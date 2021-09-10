dc9f563cf7758725ac87f7a4cf23aeb4541e78a0
Pramote Siriparp <6210530017@student.tu.ac.th>
2021-09-10 18:53:22 +0000

    create our model

--- YOKxPIERegistration/settings.py
+++ YOKxPIERegistration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g0cx+&y!(t9yk2!dtpje0@h+@x!j3v5i72l$6m5yj_3u2f09hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
