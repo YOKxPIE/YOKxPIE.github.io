e7f1b11b9bfbbb32e9986633266814b68996d00c
Pramote Siriparp <6210530017@student.tu.ac.th>
2021-09-13 07:12:59 +0000

    update

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course
+from .models import *
 
 admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Enroll)
--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,7 @@ class Course(models.Model):
     # course code ex.CN331
     c_code = models.CharField(max_length=5)
     # course name
-    c_name = models.CharField(max_length=150)
+    c_name = models.CharField(max_length=150, null = True)
     semester = models.IntegerField()
     # academic year
     a_year = models.IntegerField()
@@ -16,5 +16,19 @@ class Course(models.Model):
     # Is this course open?  default = yes
     status = models.BooleanField(default=True)
 
-def __str__(self):
-    return f"{self.c_code} {self.semester}/{self.a_year}"
+    def __str__(self):
+        return f"{self.c_code} {self.semester}/{self.a_year}"
+
+class Student(models.Model):
+    First_name = models.CharField(max_length=100, null = True)
+    Last_name = models.CharField(max_length=100, null = True)
+    email = models.CharField(max_length=200, null = True)
+    student_id = models.CharField(max_length=10, null = True)
+    
+    def __str__(self):
+        return f"{self.student_id}: {self.First_name} {self.Last_name}"
+        
+class Enroll(models.Model):
+    # student =
+    # course =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
