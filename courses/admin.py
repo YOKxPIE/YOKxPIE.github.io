ce7cfa18cb906301db3a3a709fe9ded448d5bfb4
6210612666 <6210612666@student.tu.ac.th>
2021-09-13 06:10:19 +0000

    update modelsCourses

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course
+from .models import *
 
+class StudentAdmin(admin.ModelAdmin):
+    filter_horizontal = ("course",)
+    
 admin.site.register(Course)
+admin.site.register(Student ,StudentAdmin)
+#admin.site.register(Take)
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 #from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 # Create your models here.
 class Course(models.Model):
@@ -18,19 +19,23 @@ class Course(models.Model):
     status = models.BooleanField(default=True)
 
     def __str__(self):
-<<<<<<< HEAD
-        return f"This course is {self.c_code}"
+        return f"{self.c_code} {self.semester}/{self.a_year}"
+        
+        
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    course = models.ManyToManyField(Course ,blank=True ,related_name="students")
 
+    def __str__(self):
+        return f"{self.id} : {self.name}"
+        
+        
 #class Take(models.Model):
     #t_code = models.ForeignKey(Course, on_delete=models.CASCADE)
+    #course = models.ForeignKey(Course, on_delete=models.CASCADE)
     #t_semester = models.ForeignKey(Course, on_delete=models.CASCADE)
     #t_year = models.ForeignKey(Course, on_delete=models.CASCADE)
-    #students = models.ManyToManyField(User, blank=True)
+    #student = models.ForeignKey(Student, on_delete=models.CASCADE)
     
     #def __str__(self):
-        #return f"{self.id} {self.students}: {self.t_code} {self.t_semester} {self.t_year}"
-=======
-        return f"{self.c_code} {self.semester}/{self.a_year}"
-
-
->>>>>>> 4b112a4187b032ba2a9f9b027915ce2aeb8d7b15
+        #return f"{self.id} {self.students}: {self.course}"
