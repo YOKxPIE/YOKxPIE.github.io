d51ecc6889b4121b00e27da28067977eef872d42
6210612666 <6210612666@student.tu.ac.th>
2021-09-14 09:31:02 +0000

    update user/login1

--- courses/views.py
+++ courses/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 
 
-#login users ไมได้อาจเพราะสร้างappแยกแต่เข้าผ่าน/users
+#login users ไมได้อาจเพราะสร้างappแยกแต่เข้าผ่าน/usersได้
 def index(request):
     if not request.user.is_authenticated:
         return render(request, 'users/login.html')
@@ -19,24 +19,6 @@ def index(request):
         })
     
 
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("users:index"))
-        else:
-            messages.warning(request, "Invalid credential.")
-            return render(request, "users/login.html", {
-                "messages": messages.get_messages(request)
-            })
-    return render(request, "users/login.html")
-    
-def logout_view(request):
-    logout(request)
-    
-    return HttpResponseRedirect(reverse("users:login"))
+
 
 
