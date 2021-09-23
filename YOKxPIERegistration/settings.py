6cd852676df0f30f5e110e53bb4a2f7632f192be
6210612666 <6210612666@student.tu.ac.th>
2021-09-23 15:06:42 +0000

    bug student data and haven't setting.py for heroku

--- YOKxPIERegistration/settings.py
+++ YOKxPIERegistration/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'YOKxPIERegistration.urls'
@@ -121,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ทดลอง
 MEDIA_URL = '/images/'
