44148109ed2ec1176647a1f31bdc502586afa8d0
Pramote Siriparp <6210530017@student.tu.ac.th>
2021-09-25 11:51:21 +0000

    add default max_stu and add is_seat_available

--- courses/models.py
+++ courses/models.py
@@ -8,11 +8,14 @@ class Course(models.Model):
     semester = models.IntegerField()
     a_year = models.IntegerField()
     count_stu = models.IntegerField(default=0)
-    max_stu = models.IntegerField()
+    max_stu = models.PositiveIntegerField(default=3)
     status = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.c_code} {self.semester}/{self.a_year}"
+
+    def is_seat_available(self):
+        return self.count_stu < self.max_stu
 
 
 class Student(models.Model):
@@ -24,13 +27,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.student_id}: {self.First_name} {self.Last_name}"
-        
+
 
 class Enroll(models.Model):
     student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    
-    
+
+
     def __str__(self):
         return f"{self.student} enroll {self.course}"
